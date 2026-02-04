1f5852d397526da2ced1b41f073fb34a5211f87b
shoode <vakalama@protonmail.com>
2026-02-04 19:28:14 +0200

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Taras Khanchuk on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
