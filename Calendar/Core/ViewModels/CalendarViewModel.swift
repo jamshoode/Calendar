1c223ec4575aad67f2a685bc0360a571604d7b36
shoode <vakalama@protonmail.com>
2026-02-05 13:28:00 +0200

    fix(ui): some minor changes i forgor what changed.

--- Calendar/Core/ViewModels/CalendarViewModel.swift
+++ Calendar/Core/ViewModels/CalendarViewModel.swift
@@ -4,7 +4,7 @@ import Combine
 
 class CalendarViewModel: ObservableObject {
     @Published var currentMonth: Date = Date()
-    @Published var selectedDate: Date? = nil
+    @Published var selectedDate: Date? = Date()
     
     func moveToPreviousMonth() {
         currentMonth = Calendar.current.date(byAdding: .month, value: -1, to: currentMonth) ?? currentMonth
